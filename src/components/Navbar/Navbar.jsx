9ca3d750202336f821ea6d0279d67b3fa4f2a2c3
Siddhant Tripathi <86283827+siddhanttripathi22@users.noreply.github.com>
2025-08-05 12:33:41 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   
   const handleResumeDownload = () => {
     
-    const resumeUrl = "https://drive.google.com/file/d/1k5HU6TbELRISNjLgYROaLswH_SASS89S/view?usp=sharing "; 
+    const resumeUrl = "https://drive.google.com/file/d/1M6ErnBafdNNSSFVMl4Wx0RP2VkeeWzhf/view?usp=sharing "; 
     
     
     const link = document.createElement('a');
